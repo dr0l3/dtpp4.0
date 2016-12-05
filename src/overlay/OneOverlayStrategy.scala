9a3eb267ab9ae49cc948ab9c6ed11ce5bd81e045
Pedersen <G50848@oneadr.net>
2016-12-05 09:42:13 +0100

    Doesnt jump and popup stays

--- src/overlay/OneOverlayStrategy.scala
+++ src/overlay/OneOverlayStrategy.scala
@@ -10,10 +10,18 @@ import state.PluginState
   */
 class OneOverlayStrategy(val actionStrategy: ActionStrategy) extends OverlayStrategy{
   override def handleSelect(selectedMarkerString: String, pluginState: PluginState, editor: Editor): List[PluginState] = {
-    // TODO: This does not work
-    val selectedMarker: Option[Marker] = pluginState.markerList.find(markerOpt => markerOpt.get.replacementText.toLowerCase.eq(selectedMarkerString.toLowerCase)).get
-    println(selectedMarker.get)
-    actionStrategy.performAction(editor,selectedMarker.get.startOffset)
-    List(new PluginState(new TextPopup(false, ""), List(), pluginState.selectedMarkers ::: List(selectedMarker), false, List(new SimpleListenerDescription(ListenerType.NonAccept))))
+
+    pluginState.markerList.foreach(marker => println(marker.get.replacementText))
+    val list = pluginState.markerList.filter(markerOpt => markerOpt.get.replacementText.toLowerCase.eq(selectedMarkerString.toLowerCase))
+    list.size match {
+      case 1 =>
+        // TODO: This does not work. Doesnt jump and popup stays
+        val selectedMarker: Option[Marker] = list.head
+        actionStrategy.performAction(editor,selectedMarker.get.startOffset)
+        List(new PluginState(new TextPopup(false, ""), List(), pluginState.selectedMarkers ::: List(selectedMarker), false, List(new SimpleListenerDescription(ListenerType.NonAccept))))
+      case _ =>
+        List(new PluginState(pluginState.popup, List(), pluginState.selectedMarkers, pluginState.isSelecting, pluginState.listenerList))
+    }
+
   }
 }
